2d538b0e60e906c4c8873815c39a02c3d83fc868
Your Name <you@example.com>
2024-06-06 11:26:14 +0700

    (rollback) app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import './App.css';
 
 import { BrowserRouter, Routes } from 'react-router-dom'
@@ -17,18 +15,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </Suspense>
-=======
-import { useSelector } from 'react-redux';
-import './App.css';
-import WeatherSearch from './components/WeatherSearch';
-
-function App() {
-
-  return (
-    <div>
-      <WeatherSearch />
-    </div>
->>>>>>> 57e407c0a856136c5ff4cdffd32795582e3893fd
   );
 }
 
